807c3149cacba2d91ba7b6451ceb5144930be315
shota <shouta1348@icloud.com>
2022-06-15 03:34:37 +0900

    add gitignore

--- system_prj/system_prj/settings.py
+++ system_prj/system_prj/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5z37jpw)_trqbgpjj^1^^kvuzc*ly%$3qk^&!hau^!(j^&$e4_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
